3826ee707535fd49d37bfc399b48217d60bab9a3
Morgan Grainger <morgan.grainger@gmail.com>
2023-11-25 12:04:49 -0600

    Add the first few problems.

---
+++ juliet/CWE121_Stack_Based_Buffer_Overflow__char_type_overrun_memcpy_02/func.c
@@ -0,0 +1,40 @@
+ 
+ 
+
+#include "std_testcase.h"
+
+#include <wchar.h>
+
+ 
+#define SRC_STR "0123456789abcdef0123456789abcde"
+
+typedef struct _charVoid
+{
+    char charFirst[16];
+    void * voidSecond;
+    void * voidThird;
+} charVoid;
+
+
+void func_foo()
+{
+    if(1)
+    {
+        {
+            charVoid structCharVoid;
+            structCharVoid.voidSecond = (void *)SRC_STR;
+             
+            printLine((char *)structCharVoid.voidSecond);
+             
+            memcpy(structCharVoid.charFirst, SRC_STR, sizeof(structCharVoid));
+            structCharVoid.charFirst[(sizeof(structCharVoid.charFirst)/sizeof(char))-1] = '\0';  
+            printLine((char *)structCharVoid.charFirst);
+            printLine((char *)structCharVoid.voidSecond);
+        }
+    }
+}
+
+
+
+ 
+
---
+++ juliet/CWE121_Stack_Based_Buffer_Overflow__char_type_overrun_memcpy_05/func.c
@@ -0,0 +1,44 @@
+ 
+ 
+
+#include "std_testcase.h"
+
+#include <wchar.h>
+
+ 
+#define SRC_STR "0123456789abcdef0123456789abcde"
+
+typedef struct _charVoid
+{
+    char charFirst[16];
+    void * voidSecond;
+    void * voidThird;
+} charVoid;
+
+ 
+static int staticTrue = 1;  
+static int staticFalse = 0;  
+
+
+void func_foo()
+{
+    if(staticTrue)
+    {
+        {
+            charVoid structCharVoid;
+            structCharVoid.voidSecond = (void *)SRC_STR;
+             
+            printLine((char *)structCharVoid.voidSecond);
+             
+            memcpy(structCharVoid.charFirst, SRC_STR, sizeof(structCharVoid));
+            structCharVoid.charFirst[(sizeof(structCharVoid.charFirst)/sizeof(char))-1] = '\0';  
+            printLine((char *)structCharVoid.charFirst);
+            printLine((char *)structCharVoid.voidSecond);
+        }
+    }
+}
+
+
+
+ 
+
